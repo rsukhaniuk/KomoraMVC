26a9ee11520e2b5455660a1eec7d562456707f9b
rsukhaniuk <rsukhaniuk@gmail.com>
2024-05-24 03:09:33 +0300

    calculate menu added view

--- KomoraMVC/Areas/User/Controllers/CalculateMenuController.cs
+++ KomoraMVC/Areas/User/Controllers/CalculateMenuController.cs
@@ -225,6 +225,189 @@ namespace Komora.Areas.User.Controllers
             return View(model); // Return the model to display any validation errors
         }
 
+        [HttpPost]
+        public IActionResult Insert (MenuPlanningViewModel model)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+
+
+            foreach (var menuVM in model.CalculatedMenus)
+            {
+
+                menuVM.Menu.Status = true;
+                menuVM.Menu.UserId = userId;
+
+
+                var calcOrderQuan = CalculatePlanQuan(menuVM, 1.0);
+                foreach (var item in calcOrderQuan)
+                {
+                    var inventoryItems = _unitOfWork.Inventory
+                        .GetAll(i => i.ProductId == item.ProductId)
+                        .OrderBy(i => i.ExpirationDate == DateTime.MinValue ? 1 : 0)  // Prioritize records without DateTime.MinValue
+                        .ThenBy(i => i.ExpirationDate)  // Then sort by date where not MinValue
+                        .ToList();
+
+                    var totalPlanToAdd = item.PlanQuan;
+                    foreach (var inventoryItem in inventoryItems)
+                    {
+                        if (totalPlanToAdd <= 0) break;
+
+                        var possibleToAdd = inventoryItem.IncomeQuantity - inventoryItem.PlanQuantity;
+                        if (possibleToAdd > 0)
+                        {
+                            var toAdd = Math.Min(possibleToAdd, totalPlanToAdd);
+                            inventoryItem.PlanQuantity = Math.Round(inventoryItem.PlanQuantity + toAdd, 3, MidpointRounding.AwayFromZero); ;
+                            inventoryItem.PlanDate = DateTime.Now;
+                            inventoryItem.RemainQuantity = Math.Round(inventoryItem.IncomeQuantity - inventoryItem.PlanQuantity, 3, MidpointRounding.AwayFromZero);
+                            inventoryItem.Remaindate = DateTime.Now;
+                            totalPlanToAdd -= toAdd;
+                        }
+                        _unitOfWork.Inventory.Update(inventoryItem);
+                    }
+
+                }
+
+                if (menuVM.Menu.Id == 0)
+                {
+                    _unitOfWork.Menu.Add(menuVM.Menu);
+                    _unitOfWork.Save();
+                }
+                else
+                {
+                    _unitOfWork.Menu.Update(menuVM.Menu);
+                    _unitOfWork.Save();
+                }
+
+                bool hasErrors = false;
+                string errorMessage = "";
+
+                if (menuVM.MenuRecipes != null)
+                {
+                    foreach (var menuRecipe in menuVM.MenuRecipes)
+                    {
+                        menuRecipe.MenuId = menuVM.Menu.Id;
+
+                        if (string.IsNullOrWhiteSpace(menuRecipe.RecipeId.ToString()) || menuRecipe.RecipeId == 0)
+                        {
+                            errorMessage += "Recipe is required. ";
+                            hasErrors = true;
+                        }
+
+                        // Припускаємо, що UnitId також є обов'язковим
+
+
+                        // Якщо немає помилок, додаємо або оновлюємо записи
+                        if (!hasErrors)
+                        {
+                            if (menuRecipe.Id == 0)
+                            {
+                                _unitOfWork.MenuRecipe.Add(menuRecipe);
+                            }
+                            else
+                            {
+                                _unitOfWork.MenuRecipe.Update(menuRecipe);
+                            }
+                        }
+                    }
+
+                    if (hasErrors)
+                    {
+                        TempData["error"] = errorMessage; // Зберігаємо помилки у TempData
+                    }
+                    else
+                    {
+                        _unitOfWork.Save();
+                        
+                    }
+                }
+                
+
+            }
+            TempData["success"] = "Menu added successfully.";
+
+            return RedirectToAction("Index");
+        }
+
+
+        public List<OrderVM> CalculatePlanQuan(CalculateMenuVM menuVM, double tolerance)
+        {
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var today = DateTime.Today; // Define the date limit for filtering menus.
+            var isMenuActive = menuVM.Menu.Status == true; // Check the status of the menu
+            var menuDate = menuVM.Menu.Date; // Get the date from the Menu object
+
+            if (menuDate >= today && isMenuActive)
+            {
+
+                var recipeIds = menuVM.MenuRecipes.Select(mr => mr.RecipeId).ToList(); // Extract only RecipeIds first
+
+                // Fetch necessary base data without trying to filter on the in-memory list
+                var productData = _db.Products
+                    .Select(p => new
+                    {
+                        p.Id,
+                        p.Name,
+                        CategoryName = p.Category.Name,
+                        p.Price,
+                        p.Quantity,
+                        Unit = p.Unit.Name,
+                        ProductRecipes = _db.ProductRecipe
+                            .Where(pr => pr.ProductId == p.Id && recipeIds.Contains(pr.RecipeId)) // Only take ProductRecipes that match the RecipeIds
+                            .Join(_db.Recipes,
+                                pr => pr.RecipeId,
+                                r => r.Id,
+                                (pr, r) => new { ProductRecipe = pr, Recipe = r }) // Join here is fine as it involves only DB sets
+                            .ToList(),
+                        Remains = _db.Inventory
+                            .Where(i => i.ProductId == p.Id && i.UserId == userId)
+                            .Sum(i => i.IncomeQuantity - i.PlanQuantity - i.WasteQuantity)
+                    })
+                    .ToList(); // Execute the DB query and retrieve the results
+
+                // Now apply any in-memory operations
+                var finalData = productData.Select(p => new
+                {
+                    p.Id,
+                    p.Name,
+                    p.CategoryName,
+                    p.Price,
+                    p.Quantity,
+                    p.Unit,
+                    PlanQuantitiesInfo = p.ProductRecipes
+                        .SelectMany(pr => menuVM.MenuRecipes
+                            .Where(mr => mr.RecipeId == pr.ProductRecipe.RecipeId)
+                            .Select(mr => new { mr.Servings, pr.ProductRecipe.Quantity }))
+                        .ToList(),
+                    p.Remains
+                }).ToList();
+
+
+
+                // Process the calculations in memory
+                var orders = finalData
+                    .Select(p => new OrderVM
+                    {
+                        ProductId = p.Id,
+                        ProductName = p.Name,
+                        PlanQuan = p.PlanQuantitiesInfo.Sum(x => x.Servings * x.Quantity),
+                    })
+                    .ToList();
+
+
+
+                return orders;
+            }
+            else
+            {
+                return new List<OrderVM>();
+            }
+        }
+
         //public IActionResult Index()
         //{
         //    var claimsIdentity = (ClaimsIdentity)User.Identity;
